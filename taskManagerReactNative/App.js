f46218b4e7ee9adf73aaaeb78864640ad63cd0d1
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 21:19:20 -0500

    Create NavigationContainer

--- taskManagerReactNative/App.js
+++ taskManagerReactNative/App.js
@@ -1,8 +1,21 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Login from './views/Login/Login';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <Text>Desde App</Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
