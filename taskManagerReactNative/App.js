60d8e1e85c4f263615d50d95d99932a67995102f
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 22:03:30 -0500

    Add Login page elements

--- taskManagerReactNative/App.js
+++ taskManagerReactNative/App.js
@@ -3,20 +3,11 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 import Login from './src/views/Login/Login';
 
 const Stack = createStackNavigator();
-
-// Tema de la aplicación
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-  },
-};
-console.log(theme);
 
 const App = () => {
   return (
--- taskManagerReactNative/src/views/Login/Login.js
+++ taskManagerReactNative/src/views/Login/Login.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+
+import {Button, TextInput} from 'react-native-paper';
+
+import {colors} from '../../styles/globalStyles';
 
 const Login = () => {
-  return <Text>Login</Text>;
+  return (
+    <>
+      <View>
+        <Text>
+          <Text>Welcome,</Text>
+          {'\n'}
+          Glad to see you!
+        </Text>
+      </View>
+      <View>
+        <TextInput
+          mode="outlined"
+          label="Email"
+          activeOutlineColor={colors.primary}
+        />
+        <TextInput
+          mode="outlined"
+          label="Password"
+          activeOutlineColor={colors.primary}
+        />
+      </View>
+
+      <Button mode="contained" color={colors.primary}>
+        Login
+      </Button>
+
+      <Text>
+        Don't have an account?
+        <Text>Sign up now</Text>
+      </Text>
+    </>
+  );
 };
 
 export default Login;
