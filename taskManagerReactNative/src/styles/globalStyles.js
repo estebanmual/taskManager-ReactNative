7c0a59e7ccbaf01224822c861cea6e1779b94110
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 22:03:15 -0500

    Add global colors for the app

---
+++ taskManagerReactNative/src/styles/globalStyles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+// colores de la aplicación
+const colors = {
+  primary: '#6391E7',
+  secondary: '#474F61',
+};
+
+// estilos globales
+const globalStyles = StyleSheet.create({});
+
+export {colors};
