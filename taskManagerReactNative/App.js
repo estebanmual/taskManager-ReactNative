fe75af18dddb08fb9c2c4bc3b0b4bfbd1e59d76e
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 21:32:18 -0500

    Add DefaultTheme for editing

--- taskManagerReactNative/App.js
+++ taskManagerReactNative/App.js
@@ -3,10 +3,20 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 
 import Login from './views/Login/Login';
 
 const Stack = createStackNavigator();
+
+// Tema de la aplicación
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  },
+};
+console.log(theme);
 
 const App = () => {
   return (
