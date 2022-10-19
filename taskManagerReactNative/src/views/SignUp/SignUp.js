90422b8f52603c5a2b22d148319248e6f3db789f
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-19 01:44:09 -0500

    Delete inline style for inputs

--- taskManagerReactNative/src/views/SignUp/SignUp.js
+++ taskManagerReactNative/src/views/SignUp/SignUp.js
@@ -20,59 +20,24 @@ const Login = props => {
           </Text>
         </View>
         <View style={globalStyles.inputContainer}>
-          <TextInput
-            mode="outlined"
-            label="Name"
-            style={globalStyles.input}
-            theme={{
-              colors: {
-                primary: colors.primary,
-                text: colors.secondary,
-                placeholder: colors.primary,
-              },
-              roundness: 10,
-            }}
-          />
+          <TextInput mode="outlined" label="Name" style={globalStyles.input} />
           <TextInput
             mode="outlined"
             label="Lastname"
             style={globalStyles.input}
-            theme={{
-              colors: {
-                primary: colors.primary,
-                text: colors.secondary,
-                placeholder: colors.primary,
-              },
-              roundness: 10,
-            }}
           />
           <TextInput
             mode="outlined"
             label="Username"
             style={globalStyles.input}
-            theme={{
-              colors: {
-                primary: colors.primary,
-                text: colors.secondary,
-                placeholder: colors.primary,
-              },
-              roundness: 10,
-            }}
           />
           <TextInput
             mode="outlined"
             label="Password"
             secureTextEntry={true}
             style={globalStyles.input}
-            theme={{
-              colors: {
-                primary: colors.primary,
-                text: colors.secondary,
-                placeholder: colors.primary,
-              },
-              roundness: 10,
-            }}
           />
+          <TextInput mode="outlined" label="City" style={globalStyles.input} />
         </View>
 
         <Button
