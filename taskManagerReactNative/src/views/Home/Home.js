2e119fe48e837dbbdafa5f9520cdc66b6359fb48
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-26 13:17:33 -0500

    Display image for climate component

--- taskManagerReactNative/src/views/Home/Home.js
+++ taskManagerReactNative/src/views/Home/Home.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Text, Button} from 'react-native';
+import {Text, Button, Image} from 'react-native';
 
 import Header from '../../components/Header/Header';
 
@@ -7,15 +7,11 @@ import SessionContext from '../../context/session/sessionContext';
 
 const Home = props => {
   const {navigation} = props;
-  const {logOut} = useContext(SessionContext);
-  const cerrarSesion = () => {
-    logOut();
-    navigation.navigate('Login');
-  };
+
   return (
     <>
       <Header navigation={navigation} />
-      <Button title="Cerrar sesión" onPress={() => cerrarSesion()} />
+      <Image source={require('../../assets/images/home.png')} />
     </>
   );
 };
