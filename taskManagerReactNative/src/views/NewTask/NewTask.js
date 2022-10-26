c5df59ebb91b6a068850a16a756f0bbf43de8787
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-26 18:40:40 -0500

    Added DatePicker

--- taskManagerReactNative/src/views/NewTask/NewTask.js
+++ taskManagerReactNative/src/views/NewTask/NewTask.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Pressable} from 'react-native';
 
 import {TextInput} from 'react-native-paper';
+import {DateTimePickerAndroid} from '@react-native-community/datetimepicker';
 
 import {globalStyles, theme} from '../../styles/globalStyles';
 
 const NewTask = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+  const showDatePicker = () => {
+    DateTimePickerAndroid.open({
+      value: date,
+      mode: 'date',
+      display: 'default',
+      onChange: onChange,
+    });
+  };
   return (
     <View style={styles.viewContainer}>
       <View style={styles.container}>
@@ -18,6 +34,15 @@ const NewTask = () => {
             multiline={true}
             numberOfLines={4}
           />
+          <Pressable onPress={showDatePicker}>
+            <TextInput
+              mode="outlined"
+              label="Date"
+              style={globalStyles.input}
+              editable={false}
+              value={date.toLocaleDateString()}
+            />
+          </Pressable>
         </View>
       </View>
     </View>
@@ -34,6 +59,11 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.background,
     alignSelf: 'center',
     paddingTop: 20,
+  },
+  modalContainer: {
+    height: '50%',
+    width: '85%',
+    alignSelf: 'center',
   },
 });
 
