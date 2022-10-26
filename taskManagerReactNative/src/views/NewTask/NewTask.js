89c993f4965543742089cf13b86c23d02463d05c
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-26 15:04:52 -0500

    Add inputs in NewTask view

--- taskManagerReactNative/src/views/NewTask/NewTask.js
+++ taskManagerReactNative/src/views/NewTask/NewTask.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+
+import {TextInput} from 'react-native-paper';
+
+import {globalStyles, theme} from '../../styles/globalStyles';
 
 const NewTask = () => {
-  return <Text>NewTask</Text>;
+  return (
+    <View style={styles.viewContainer}>
+      <View style={styles.container}>
+        <View style={globalStyles.inputContainer}>
+          <TextInput mode="outlined" label="Title" style={globalStyles.input} />
+          <TextInput
+            label="Description"
+            mode="outlined"
+            style={[globalStyles.input, {height: 100}]}
+            multiline={true}
+            numberOfLines={4}
+          />
+        </View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    backgroundColor: theme.colors.background,
+    flex: 1,
+  },
+  container: {
+    width: '85%',
+    backgroundColor: theme.colors.background,
+    alignSelf: 'center',
+    paddingTop: 20,
+  },
+});
 
 export default NewTask;
